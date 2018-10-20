41196a1421c215887f8e5901e5345d5682438646
pro101noob <37694795+pro101noob@users.noreply.github.com>
2018-10-20 04:04:06 +0000

    fixing someshit
    fixing someshit

--- secs.c
+++ secs.c
@@ -2,17 +2,17 @@
 
 int main(void) {
 
-  //declaration----------
+  //declaration-----------
   int N, K;
 
-  //reading--------------
+  //reading---------------
   if ( scanf("%d%d", &N, &K) != 2 ) { printf("%d %d", -1, -1); return 0; };
   if ( N <= 0 ) { printf("%d %d", -1, -1); return 0; }
   int arrr[N];
   for ( int i=0; i<N; i++ ) { scanf("%d", &arrr[i]); }
   //for ( int i=0; i<=N-1; i++ ) { printf("%d", arrr[i]); }
 
-  //other shitful code---
+  //other shitful code----
   int first = -1, last = -1;
   for ( int i=0, b=1; i<=N && b; i++ ) { 
     if ( arrr[i] == K ) { 
